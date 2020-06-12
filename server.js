66c489ab09516e718afd4a5a4625365dee9fa440
rudatorquato <rudatorquato@gmail.com>
2020-06-11 22:15:41 -0300

    Model product

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 //Inicia o App
 const app = express();
@@ -9,9 +10,19 @@ mongoose.connect('mongodb://localhost:27017/nodeapi',
 { useNewUrlParser: true, useUnifiedTopology: true}
 );
 
+requireDir('./src/models/');
+
+const Product = mongoose.model('Product');
+
 //Primeira Rota
 app.get('/', (req, res) => {
-    res.send("FDEU CERTO");
+    Product.create({
+        title: 'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    });
+
+    return res.send("FOI");
 });
 
 app.listen(3001);
