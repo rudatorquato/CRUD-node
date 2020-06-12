109656c4e950911ab8f8f46eb7e8fbc562c5997d
rudatorquato <rudatorquato@gmail.com>
2020-06-12 02:10:13 -0300

    Paginate

--- src/models/Product.js
+++ src/models/Product.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
+const mongoosePaginete = require('mongoose-paginate');
 
 const ProductSchema = new mongoose.Schema({
     title: {
@@ -18,5 +19,7 @@ const ProductSchema = new mongoose.Schema({
         default: Date.now,
     }
 });
+
+ProductSchema.plugin(mongoosePaginete);
 
 mongoose.model("Product", ProductSchema);
